fa77e83363dcb51e3d360fe638ac318d223752d7
mirgags <markmiraglia@gmail.com>
2016-06-24 16:51:19 -0700

    slack post with colors and stuff

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const QS = require('querystring');
 const EXPRESS = require('express');
 const APP = EXPRESS();
 
+/* Set the server environment */
+
 const HOSTNAME = '10.0.0.116';
 const PORT = 8765;
+
+/* Initialize a global variable for the configuration */
+
 var CONFIG;
+
+/* Async File Read to get configuration */
 
 function getConfig (callback) {
   FS.readFile('./config.json', 'utf8', (err, data) => {
@@ -18,15 +25,19 @@ function getConfig (callback) {
   });
 };
 
+/* Wrapped for testing within callbacks */
+
 function logConfig() {
   console.log(CONFIG.slackUrl);
 };
 
-function postSlack() {
+/* Receives a stringified JSON object (maybe I should pass a JSON
+   object instead) in the format required by Slack
+   and a callback parameter */
+
+function postSlack(thePost, callback) {
   var theUrl = URL.parse(CONFIG.slackUrl);
-  var thePost = JSON.stringify({
-    "text": "Mark's Testing"
-  });
+
   var options = {
     "protocol": theUrl.protocol,
     "hostname": theUrl.host,
@@ -51,19 +62,78 @@ function postSlack() {
   req.on('error', (e) => {
     console.log(`problem with request: ${e.message}`);
   });
-
+  callback();
   req.write(thePost);
   req.end();
+};
+
+/* Writes the state to the configuration object stored in a local file,
+   this could be substitued for a DB in the future */
+
+function writeConfig() {
+  FS.writeFile('config.json', JSON.stringify(CONFIG, null, 2), (err) => {
+  if (err) throw err;
+    console.log('It\'s saved!');
+  });
 };
 
 getConfig(logConfig);
 
 APP.get('/', (req, res) => {
+  var slackText = ""; 
+  var reqUrl = URL.parse(req.url);
+/*  console.log(reqUrl); */
+  var reqQuery = QS.parse(reqUrl.query);
+/*  console.log(reqQuery); */
+
+  var slackPost = {
+    "username": "BathroomBot",
+    "attachments": [{
+      "color": "#000000",
+      "text": "",
+      "fallback": "",
+      "author_name": "Mark's PoopBot",
+      "author_icon": ":poop:"
+    }]
+  };
+
+  switch (reqQuery.value1) {
+    case "ESP_TEST":
+      slackPost.attachments[0].text += "1st Floor North Washroom is ";
+      CONFIG.bathroomKeys.br1a = reqQuery.value3;
+      break;
+    case "ESP_TEST2":
+      slackPost.attachments[0].text += "1st Floor South Washroom is ";
+      CONFIG.bathroomKeys.br1b = reqQuery.value3;
+      break;
+    case "ESP_TEST3":
+      slackPost.attachments[0].text += "2nd Floor Washroom is ";
+      CONFIG.bathroomKeys.br2 = reqQuery.value3;
+      break;
+    default:
+      console.log("no cases found");
+  };
+  slackPost.attachments[0].text += reqQuery.value3;
+  slackPost.attachments[0].fallback = slackPost.attachments.text;
+  switch (reqQuery.value3) {
+    case "occupied":
+      slackPost.attachments[0].color = "#FF0000";
+      break;
+    case "vacant":
+      slackPost.attachments[0].color = "#00FF00";
+      break;
+    default:
+      slackPost.attachments[0].color = "#BBBBBB";
+      break;
+  };
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  postSlack();
-  res.end('Hi World\n');
+  postSlack(JSON.stringify(slackPost), writeConfig);
+  console.log(JSON.stringify(CONFIG.bathroomKeys));
+  res.end('Thanks, little node mcu!\n');
 });
+
 
 APP.listen(PORT, HOSTNAME, () => {
   console.log(`Listening on ${HOSTNAME}:${PORT}`);
