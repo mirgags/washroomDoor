38bbd851e6ca226f448f7d5a46a6340212ec3a4e
mirgags <markmiraglia@gmail.com>
2016-06-24 09:30:57 -0700

    post to slack working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const NET = require('net');
 const FS = require('fs');
 const HTTPS = require('https');
+const URL = require('url');
+const QS = require('querystring');
 const EXPRESS = require('express');
 const APP = EXPRESS();
 
@@ -11,28 +13,55 @@ var CONFIG;
 function getConfig (callback) {
   FS.readFile('./config.json', 'utf8', (err, data) => {
     if(err) throw err;
-    CONFIG = data.toString();
+    CONFIG = JSON.parse(data);
     callback();
   });
 };
 
 function logConfig() {
-  console.log(CONFIG);
+  console.log(CONFIG.slackUrl);
+};
+
+function postSlack() {
+  var theUrl = URL.parse(CONFIG.slackUrl);
+  var thePost = JSON.stringify({
+    "text": "Mark's Testing"
+  });
+  var options = {
+    "protocol": theUrl.protocol,
+    "hostname": theUrl.host,
+    "path": theUrl.path,
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "Content-Length": thePost.length
+    }
+  }
+  var req = HTTPS.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.')
+    })
+  });
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+  });
+
+  req.write(thePost);
+  req.end();
 };
 
 getConfig(logConfig);
 
-
-/*
-var server = net.createServer(function(socket) {
-    socket.write('Echo server\r\n');
-    socket.pipe(socket);
-});
-*/
-
 APP.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  postSlack();
   res.end('Hi World\n');
 });
 
