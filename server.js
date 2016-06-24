370362ab28698c5bade52d51db0af657c0fc89af
mirgags <markmiraglia@gmail.com>
2016-06-23 17:41:45 -0700

    serverup

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const NET = require('net');
-const HTTP = require('http');
+const FS = require('fs');
+const HTTPS = require('https');
+const EXPRESS = require('express');
+const APP = EXPRESS();
 
-const HOSTNAME = '192.168.200.20';
+const HOSTNAME = '10.0.0.116';
 const PORT = 8765;
+var CONFIG;
+
+function getConfig (callback) {
+  FS.readFile('./config.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    CONFIG = data.toString();
+    callback();
+  });
+};
+
+function logConfig() {
+  console.log(CONFIG);
+};
+
+getConfig(logConfig);
+
 
 /*
 var server = net.createServer(function(socket) {
@@ -11,10 +30,12 @@ var server = net.createServer(function(socket) {
 });
 */
 
-var server = HTTP.createServer((req, res) => {
+APP.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hi World\n');
 });
 
-server.listen(PORT, HOSTNAME);
+APP.listen(PORT, HOSTNAME, () => {
+  console.log(`Listening on ${HOSTNAME}:${PORT}`);
+});
